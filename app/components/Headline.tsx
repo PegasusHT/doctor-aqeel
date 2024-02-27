29e622f237a214df87c0db1fa17e506aa3075d20
jimmyDev <jimmybuidev@gmail.com>
2024-02-27 06:29:42 -0800

    init headline

---
+++ app/components/Headline.tsx
@@ -0,0 +1,23 @@
+
+const Headline = () => {
+    return (
+        <div className="flex w-full text-center">
+            <div className="w-1/3 mt-[-50px] ml-[-3vw]">
+                <a href='https://dr-aqeel.com/'>
+                    <img src="https://dr-aqeel.com/wp-content/uploads/2024/01/Dr.aqeel-farooque-logo-png-01-1024x1024.png" className="" alt=""/>
+                </a>      
+            </div>
+            <div className="w-3/5 ">
+                <p>
+                    Dr Aqeel Farooque
+                    <br></br>
+                    MBBS CCT (UK) FRCPCH (UK)
+                </p>
+            </div>
+            <div className="w-1/5"> links </div>
+        </div>
+
+    )
+}
+
+export default Headline;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Headline from "./components/Headline";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen ">
+      <Headline />
+    </main>
+  );
+}
