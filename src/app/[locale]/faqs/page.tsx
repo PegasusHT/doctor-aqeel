38bb75fc56978e71b3afa73fa9aeb6c6afbb756a
jimmyDev <jimmybuidev@gmail.com>
2024-03-27 17:07:00 -0700

    completed faqs

--- src/app/[locale]/faqs/page.tsx
+++ src/app/[locale]/faqs/page.tsx
@@ -8,7 +8,7 @@ const FAQs: React.FC = () => {
         <div className='mb-14 mt-5' >
             <div className="relative flex items-center justify-center h-64 mb-12" style={{ backgroundColor: '#b9cfd8' }}>
                 <div className="relative z-10 flex flex-col justify-center items-center text-lg font-bold">
-                    <h2 className='text-6xl' >FAQs</h2>
+                    <h2 className='text-6xl ' >FAQs</h2>
                 </div>
             </div>
        
@@ -16,7 +16,7 @@ const FAQs: React.FC = () => {
                 <h1 className='text-2xl font-bold'> Frequently asked questions </h1>
             </div>
 
-            <p className='ml-3 mt-10 text-lg font-semibold'> FAQs </p>
+            <p className='ml-3 mt-10 text-lg font-semibold mb-2'> FAQs </p>
 
             {
                 FaqData.map((faq, index) => (
