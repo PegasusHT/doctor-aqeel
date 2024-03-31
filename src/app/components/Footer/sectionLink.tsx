6f0e5f8fbc795fbd151a003c84fd467b653fd8c3
jimmyDev <jimmybuidev@gmail.com>
2024-03-30 18:21:53 -0700

    connect links to footer nav

---
+++ src/app/components/Footer/sectionLink.tsx
@@ -0,0 +1,38 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { useLocale } from 'next-intl';
+import { usePathname } from 'next/navigation';
+
+const SectionLink = ({ sections }: { sections: string[] }) => {
+
+    const localActive = useLocale();
+    const pathname = usePathname();
+
+    return (
+        <>
+            {sections.map((section) => {
+                let expectedPathname;
+                let linkHref;
+
+                linkHref = `/${localActive}/${section.toLowerCase()}`;
+
+                expectedPathname = `/${localActive}/${section.toLowerCase()}`;
+                linkHref = `/${localActive}/${section.toLowerCase()}`;
+
+                const isActive = pathname === expectedPathname;
+
+                return (
+                    <a key={section}>
+                        <Link href={linkHref}>
+                            <span className={isActive ? 'text-rose-600 active-link' : 'text-black'}>{section}</span>
+                        </Link>
+                    </a>
+                );
+            })}
+        </>
+    );
+};
+
+export default SectionLink;
+
