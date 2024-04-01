aaab53c080d4f8d1a02df31aa9f49ff6927591b3
jimmyDev <jimmybuidev@gmail.com>
2024-03-31 18:29:58 -0700

    current change

--- src/app/components/Navbar/Headline.tsx
+++ src/app/components/Navbar/Headline.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 const Headline = ({ title }: { title: string }) => {
 
     return (
-        <div className="flex pd-0 text-center items-center xl:gap-20"> 
-            <div className="">
+        <div className="flex text-center items-center lg:w-screen"> 
+            <div className="w-full flex items-center justify-center">
                 <a href="https://dr-aqeel.com/">
                     <img
                         src="https://dr-aqeel.com/wp-content/uploads/2024/01/Dr.aqeel-farooque-logo-png-01-1024x1024.png"
@@ -14,11 +14,11 @@ const Headline = ({ title }: { title: string }) => {
                     />
                 </a>
             </div>
-            <div className="">
+            <div className="w-full ">
                 <h2 className="leading-5 font-semibold text-2xl md:text-4xl mt-5">{title}</h2>
                 <h2 className="font-normal mt-4 text-base">MBBS CCT (UK) FRCPCH (UK)</h2>
             </div>
-            <div className='pl-10 mr-auto'>
+            <div className='pl-10 lg:w-full lg:flex items-center justify-center'>
                 <LinkList position='head'/>
             </div>
         </div>
